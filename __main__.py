1381bacb64798eb62277fb5b8b07971d933de7df
marco.weger <m.weger@tsinformatica.com>
2020-11-19 03:29:17 +0100

    Config GUI

--- __main__.py
+++ __main__.py
@@ -2,6 +2,7 @@
 # pipimport.install()
 # pip3 install riotwatcher
 # pip install pandas
+# pip install Pillow
 
 import tkinter
 from tkinter import *
@@ -10,18 +11,16 @@ import os
 import __listener__
 import __lol__
 from __lol__ import Regions
-# from riot_observer import RiotObserver
-# from riot_observer import constants as riot_cost
-# from riot_observer import LoLException
-# from riot_observer.observer import error_404, error_429
+import PIL
+from PIL import ImageTk, Image
 import time
 
 # TODO
-# - every second i call API
+# - every 2 or 3 second i call API
 # - timer
 
 heroesSize = 55
-heroes = ['H0','H1','H2','H3','H4'] # TODO is usefull?
+heroes = ['H0','H1','H2','H3','H4']
 
 class window:
 
@@ -58,12 +57,96 @@ class window:
     print(button) #debug message
     # self.label.config(text = selection)
 
+def showConfigWindow():
+  global key_sv
+  global user_sv
+  global entry
+
+  entry = Tk()
+  entry.resizable(False, False)
+  entry.attributes('-topmost', True)
+
+  #background
+  try:
+    img = PIL.ImageTk.PhotoImage(Image.open(os.path.join(os.path.dirname(__file__), "bg.png")))
+    bg_width = img.width();
+    bg_height = img.height();
+    img_set = True
+  except Exception:
+    print("Can't open background image")
+    bg_width = "400";
+    bg_height = "180";
+    img_set = False
+
+  canvas = Canvas(
+    entry,
+    width = int(bg_width)-2,
+    height = int(bg_height)-2,
+    border = 0,
+    relief = "solid",
+  )
+
+  if img_set:
+    try:
+      canvas.create_image(0, 0, anchor=NW, image=img)
+    except Exception:
+      print("Can't elaborate background image")
+
+  key_sv = StringVar()
+  key_e = Entry(
+    canvas,
+    bd = 1,
+    relief = "groove",
+    textvariable = key_sv,
+    width = 44,
+    justify = 'center'
+  )
+  key_e.config({"background": "#bbb"})
+  key_e.bind('<Return>', func)
+  key_e.place(x=200, y=75, anchor="center")
+  key_e.delete(0,END)
+  key_e.insert(0,key)
+
+  user_sv = StringVar()
+  user_e = Entry(
+    canvas,
+    bd = 1,
+    relief = "groove",
+    textvariable = user_sv,
+    width = 44,
+    justify = 'center'
+  )
+  user_e.config({"background": "#bbb"})
+  user_e.bind('<Return>', func)
+  user_e.place(x=200, y=150, anchor="center")
+  user_e.delete(0,END)
+  user_e.insert(0,user)  
+  
+  canvas.grid(row=0,column=0)
+  
+  entry.overrideredirect(True)
+  center(entry)
+
+  key_e.focus()
+  key_e.select_range(0,'end')
+
+  entry.mainloop()
+
 def func(event):
-  # TODO... Connection test
-  key = sv.get()
+  global key
+  global user
+  global entry
+
+  key = key_sv.get()
   text_file = open(os.path.join(os.path.dirname(__file__), 'key.config'), "w")
   text_file.write(key)
   text_file.close()
+
+  user = user_sv.get()
+  text_file = open(os.path.join(os.path.dirname(__file__), 'user.config'), "w")
+  text_file.write(user)
+  text_file.close()
+
   entry.destroy()
 
 def center(root):
@@ -82,82 +165,70 @@ if __name__ == "__main__":
     fileObject = open(os.path.join(os.path.dirname(__file__), 'key.config'), "r")
     key = fileObject.read()
   except:
-    key = ''
+    key = 'API key'
+  # user management
+  try:
+    fileObject = open(os.path.join(os.path.dirname(__file__), 'user.config'), "r")
+    user = fileObject.read()
+  except:
+    user = 'Username'
 
-  entry = Tk()
-  
-  entry.resizable(False, False)
-  entry.attributes('-topmost', True)
-  entry.title("")
-  entry.iconbitmap(default=os.path.join(os.path.dirname(__file__), 'transparent.ico'))
-
-  l = Label(entry, text = "API Key")
-  l.pack(side = TOP, padx = 10, pady = 10)
-
-  sv = StringVar()
-  e = Entry(entry, bd = 5, textvariable = sv)
-  e.bind('<Return>', func)
-  e.pack(side = BOTTOM, padx = 10, pady = 10)
-  e.delete(0,END)
-  e.insert(0,key)
-  # TODO select all
-
-  # TODO onClose
-
-  # TODO border
-  
-  entry.overrideredirect(True)
-  center(entry)
-
-  entry.mainloop()
-
-  # start main
-
-  top = tkinter.Tk()
-  heroesSize = int(top.winfo_screenheight()/20)
-
-  th = __listener__.listener()
-  th.start()
-
-  win = window(top)
-  top.geometry(str(int(heroesSize*1.5))+"x"+str(heroesSize*5))
-  top.overrideredirect(1) #Remove border
-  top.resizable(False, False)
-  top.attributes('-topmost', True)
-
-  ###########
-
-  TMP_NAME = 'Throw'
+  showConfigWindow()
 
   # TODO prevedere errore inserimento nome
   watcher = __lol__.RiotObserver(key,Regions.EUROPE_WEST)
-  me = watcher.get_summoner_by_name(summoner_name=TMP_NAME)
-  time.sleep(1)
-  game = watcher.get_current_game(me['id'])
-
   try:
-    game['participants']
-  except:
-    print("Not in a game...")
+    me = watcher.get_summoner_by_name(summoner_name=user) # This requeset is used as a 'login', is it fails i kill the process because username or key is broken
+    if 'status' in me:
+      if 'status_code' in me['status']:
+        if int(me['status']['status_code']) >= 300:
+          raise ValueError("Invalid username or API key")
+  except Exception:
+    raise ValueError(Exception)
   else:
-    myteam = [x for x in game['participants'] if x['summonerName'] == TMP_NAME][0]['teamId']
-    challengers = [x for x in game['participants'] if x['teamId'] != myteam]
-    for c in challengers: # x['championId'],x['spell1Id'],x['spell2Id']}
-      print(watcher.get_champion_by_id(c['championId']))
+    # start main
+    top = tkinter.Tk()
+    heroesSize = int(top.winfo_screenheight()/20)
 
-  ## TODO maybe image from data dragon
+    th = __listener__.listener()
+    th.start()
 
-  while True:
-    # window update
-    top.update_idletasks()
-    top.update()
-    # show/hide
-    if __listener__.listener.isShowed:
-      top.deiconify()
+    win = window(top)
+    top.geometry(str(int(heroesSize*1.5))+"x"+str(heroesSize*5))
+    top.overrideredirect(1) #Remove border
+    top.resizable(False, False)
+    top.attributes('-topmost', True)
+
+    ##############################################
+    # TODO FROM HERE MUST START WITH A TIMER
+    time.sleep(1)
+    game = watcher.get_current_game(me['id'])
+
+    try:
+      game['participants']
+    except:
+      print("Not in a game...")
     else:
-      top.withdraw()
-    if not th.isAlive:
-      break    
+      myteam = [x for x in game['participants'] if x['summonerName'] == TMP_NAME][0]['teamId']
+      challengers = [x for x in game['participants'] if x['teamId'] != myteam]
+      for c in challengers: # x['championId'],x['spell1Id'],x['spell2Id']}
+        print(watcher.get_champion_by_id(c['championId']))
+
+    # TODO maybe image from data dragon
+
+    while True:
+      # window update
+      top.update_idletasks()
+      top.update()
+      # show/hide
+      if __listener__.listener.isShowed:
+        top.deiconify()
+      else:
+        top.withdraw()
+      if not th.isAlive:
+        break    
+    # END TIMER
+    ##############################################
 
   top.destroy()
   top.quit()
